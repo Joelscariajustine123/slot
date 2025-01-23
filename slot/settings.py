c342bafdd6b67d382ec18b3c087a2d0da3bbd5c6
Joel scaria Justine <84953776+Joelscariajustine123@users.noreply.github.com>
2025-01-23 13:43:03 +0530

    Update settings.py

--- slot/settings.py
+++ slot/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'slot.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-      'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  # Database name
-        'USER': 'postgres',  # Username
-        'PASSWORD': 'gtec12345678',  # Replace with your password
-        'HOST': 'db.qipgxmxohtpkxbsvtrih.supabase.co',  # Host
-        'PORT': '5432',  # Port
+      ' 'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
